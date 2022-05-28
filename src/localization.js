377766cea678f5de3e40740c5ce0910d85a831e9
nemanjaNakomcic <nemanja.nakomcic@lilly021.com>
2022-05-28 12:00:56 +0200

    initial commit

---
+++ src/localization.js
@@ -0,0 +1,13 @@
+import LocalizedStrings from "localized-strings";
+
+let strings = new LocalizedStrings({
+    sr: {
+        header: {
+            logout: 'Izlogujte se'
+        },
+        home:{
+            login: 'Ulogujte se',
+            userLogin: 'Nastavite kao gost',
+        }
+    }
+})
